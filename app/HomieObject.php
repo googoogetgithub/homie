c7da2147f483a66f52d7c0fbc09dd05b6abec5b6
Googooget Github <googooget.github@gmail.com>
2019-01-14 18:05:59 +0700

    commit before going home on Monday

--- app/HomieObject.php
+++ app/HomieObject.php
@@ -4,12 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class HomieObject extends Model
 {
     //
+    public $timestamps = false;
 
-
-
+    
 
     function getResidentType() {
         return $this->residentType;
--- database/migrations/2019_01_14_084355_create_homie_objects_table.php
+++ database/migrations/2019_01_14_084355_create_homie_objects_table.php
@@ -18,73 +18,73 @@ class CreateHomieObjectsTable extends Migration
             $table->increments('id');
 
             // internal generated code
-            $table->string('propertyCode');
-            $table->string('propertyName' , 255);
+            $table->string('propertyCode')->default('')->nullable();
+            $table->string('propertyName' , 255)->default('')->nullable();
 
-            $table->string('residentType');  // land , house + land , houses + land , apartment,  condominium , rooms , office , building
+            $table->string('residentType')->default('unknown')->nullable();  // unknown , land , house + land , houses + land , apartment,  condominium , rooms , office , building
 
-            $table->string('realEstateName', 50);
+            $table->string('realEstateName', 50)->default('')->nullable();
 
 
             // shared structured, like rooms within the same building
-            $table->string('sharedStructureCode' , 30);
-            $table->string('sharedStructureName' , 100);
+            $table->string('sharedStructureCode' , 30)->default('')->nullable();
+            $table->string('sharedStructureName' , 100)->default('')->nullable();
 
 
 
-            $table->boolean('isForSale');
-            $table->decimal('saleAmount' ,15 , 2);
-            $table->boolean('isForRent');            
-            $table->decimal('rentRate' ,15 , 2);
+            $table->boolean('isForSale')->default(false)->nullable();
+            $table->decimal('saleAmount' ,15 , 2)->nullable();
+            $table->boolean('isForRent')->default(false)->nullable();            
+            $table->decimal('rentRate' ,15 , 2)->nullable();
 
 
 
-            $table->longText('description');
+            $table->longText('description')->nullable();
 
-            $table->string('district' , 30);
-            $table->string('province' , 30);
-            $table->string('postCode' , 5 );
+            $table->string('district' , 30)->nullable();
+            $table->string('province' , 30)->nullable();
+            $table->string('postCode' , 5 )->nullable();
 
-            $table->longText('notes');
+            $table->longText('notes')->nullable();
 
 
             
             // calculable fields
-            $table->decimal('latLong' , 9 , 6);
+            $table->decimal('latLong' , 9 , 6)->nullable();
 
-            $table->decimal('ratings');
-            $table->decimal('raters');
+            $table->decimal('ratings')->nullable();
+            $table->decimal('raters')->nullable();
 
-            $table->decimal('views');
-            $table->decimal('wishlisted');
+            $table->decimal('views')->nullable();
+            $table->decimal('wishlisted')->nullable();
             
 
 
             //staff  owner fields 
-            $table->string('homieGroupOwner' , 30);
-            $table->string('homiePrimaryOwner' , 30);
+            $table->string('homieGroupOwner' , 30)->nullable();
+            $table->string('homiePrimaryOwner' , 30)->nullable();
 
             // applying custom template?
-            $table->string('customTemplate' , 50);
+            $table->string('customTemplate' , 50)->nullable();
 
 
             // about displaying stuff
 
-            $table->string('pubishedStatus' , 30); // active , inactive ?
-            $table->datetime('autoPublishedFrom');
-            $table->datetime('autoPublishedTo');
-            $table->datetime('publishedOn');
-            $table->string('publishedBy' , 30);
+            $table->string('pubishedStatus' , 30)->default('inactive'); // active , inactive ?
+            $table->datetime('autoPublishedFrom')->nullable();
+            $table->datetime('autoPublishedTo')->nullable();
+            $table->datetime('publishedOn')->nullable();
+            $table->string('publishedBy' , 30)->nullable();
             
 
             // system fields 
-            $table->timestamps('createdOn');
-            $table->string('createdBy' , 30);
-            $table->timestamps('updatedOn');
-            $table->string('updatedBy' , 30);
-            $table->softDeletes('deletedOn');
-            $table->string('deletedBy' , 30);            
-            $table->longText('comments');
+            $table->datetime('createdOn')->nullable();
+            $table->string('createdBy' , 30)->default('');
+            $table->datetime('updatedOn')->nullable();
+            $table->string('updatedBy' , 30)->default('');
+            $table->softDeletes('deletedOn')->nullable();
+            $table->string('deletedBy' , 30)->default('');            
+            $table->longText('comments')->nullable();
 
             
         });
